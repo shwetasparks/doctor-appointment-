23ff042031d7bbddcedb32381c70df70eb9c1a2b
Shweta <shwetakoffficial@gmail.com>
2024-09-07 21:31:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+
+  function clicked(){
+    console.log("Button clicked");
+  }
+
+
+
+  return (
+    <div>
+      <h1>My App</h1>
+      <button className="p-4">
+        click
+      </button>
+      
+    </div>
+  )
+}
+
+export default App
